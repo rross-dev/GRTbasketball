25f4afae69c48b059a7b70b1d5bd9bd4ac0c671f
rross-dev <ryanross.dev@gmail.com>
2025-05-29 20:40:07 -0500

    hamburger edits, smoother js

--- hamburger.js
+++ hamburger.js
@@ -4,9 +4,22 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-menu .nav-item a").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    });
+});
+
+//close when clicked outside hamburger
+document.addEventListener("click", function(event) {
+  const isClickInsideMenu = navMenu.contains(event.target);
+  const isClickOnHamburger = hamburger.contains(event.target);
+
+  if (!isClickInsideMenu && !isClickOnHamburger) {
     hamburger.classList.remove("active");
-    hamburger.classList.remove("active");
-}))
+    navMenu.classList.remove("active");
+  }
+});
